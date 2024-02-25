150721d4a231e12ed21e69eb1c6fbe755d4527dd
Administrator <administrator@Salauddins-MacBook-Pro.local>
2024-02-25 21:43:04 +0530

    addition

--- trapezoid.java
+++ trapezoid.java
@@ -1,1 +1,10 @@
 import java.util.scanner;
+class trapezoid
+{
+    public static void main(String args[])
+    {
+        int A = 2, B = 4, sum;
+        sum = A + B;
+        System.out.println("the sum of two number is: "+sum);
+    }
+}

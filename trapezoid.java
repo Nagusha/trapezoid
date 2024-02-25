a4e25efcccc934c443b5823292b4dc1ae857f42d
Administrator <administrator@Salauddins-MacBook-Pro.local>
2024-02-25 21:32:44 +0530

    division

--- trapezoid.java
+++ trapezoid.java
@@ -1,1 +1,17 @@
 import java.util.scanner;
+ 
+public class trapezoid {
+	public static void main(String args[])
+    {
+        int a, b, add,division;
+        float divide;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter two number: ");
+        a=sc.nextInt();
+        b=scc.nextInt();
+        add = a + b;
+        divide = (float) a / b;
+        System.out.println("sum =" + add);
+        System.out.println("division = " + divide);
+    }
+}

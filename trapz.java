5aa7a8d7afc9290e4645b37067fcbfcdd60a51da
Administrator <administrator@Salauddins-MacBook-Pro.local>
2024-02-26 08:05:50 +0530

    addition

--- trapz.java
+++ trapz.java
@@ -1,10 +1,20 @@
 import java.util.Scanner;
 public class trapz {
-    public static void main(String[] args) {
-        // Calculate the result of the division 50/3
-        int result = 50 / 3;
-
-        // Print the result of the division
-        System.out.println(result);
+        public static void main(String[] args) {
+            // Given value
+            int value = 10;
+            // Given factor
+            int h = 3;
+            
+            // Multiply the given value by h
+            int result = multiplyByH(value, h);
+            
+            // Print the result
+            System.out.println("Result of multiplying " + value + " by " + h + ": " + result);
+        }
+        
+        public static int multiplyByH(int num, int h) {
+            return num * h;
+        }
     }
-} 
+    

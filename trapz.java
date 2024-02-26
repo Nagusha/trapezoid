1cfde16267cf1acfbad555629d4a1ecfeda434ae
Administrator <administrator@Salauddins-MacBook-Pro.local>
2024-02-26 07:59:24 +0530

    multiplication

---
+++ trapz.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class trapz {
+        public static void main(String[] args) {
+            // Given values
+            int a = 5;
+            int b = 7;
+            
+            // Add the given values
+            int result = add(a, b);
+            
+            // Print the result
+            System.out.println("Result of adding " + a + " and " + b + ": " + result);
+        }
+        
+        public static int add(int num1, int num2) {
+            return num1 + num2;
+        }
+    }
+

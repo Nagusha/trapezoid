9d2eee9d4652f1de18bb1a8b5cddf56ae67d79ed
Administrator <administrator@Salauddins-MacBook-Pro.local>
2024-02-26 07:50:56 +0530

    division

--- trapz.java
+++ trapz.java
@@ -1,1 +1,10 @@
 import java.util.Scanner;
+public class trapz {
+    public static void main(String[] args) {
+        // Calculate the result of the division 50/3
+        int result = 50 / 3;
+
+        // Print the result of the division
+        System.out.println(result);
+    }
+} 
